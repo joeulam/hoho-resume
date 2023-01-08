17f6e26d3242629f0433e91a208fd603b033e052
joeulam <joeulam345@gmail.com>
2023-01-07 23:00:37 -0500

    new update

---
+++ pages/_app.js
@@ -0,0 +1,33 @@
+import css from '../styles/home.css'
+export default function Home() {
+  return (
+    <div class="homediv">
+
+      <container class="namecard">
+        <div class="firstcard">
+          <h1>Joey Lam</h1>
+          <h3>A programmer and a student</h3>
+        </div>
+        
+      </container>
+      
+      <container class="namecard">
+        <div class="firstcard">
+          <h2>Here is some of my projects</h2>
+        </div>
+
+        <div class="projectcon">
+
+        </div>
+      </container>
+
+      <container class="namecard">
+        Im currently working on
+      </container>
+
+    </div>
+  )
+    
+    
+   
+}
